79f2a1ccccba7538b7682c01506d0168e7240ed7
kle <thenum6er078@gmail.com>
2025-07-28 22:51:47 +0800

    [WSL GNU Emacs环境]: 去除了org-download在WSL环境下

---
+++ archive/org-download.el
@@ -0,0 +1,8 @@
+;; ========================================
+;; 设置org-download
+;; ========================================
+;; 依赖xclip，同时截图工具如(spectacle)必须设置为复制截图到剪贴板或按下C-c来复制
+;(use-package org-download
+;  :ensure t
+;  :defer nil
+;  :after org)
--- elisp/note/orgmode/note-orgmode.el
+++ elisp/note/orgmode/note-orgmode.el
@@ -36,16 +36,6 @@
 
 
 ;; ========================================
-;; 设置org-download
-;; ========================================
-;; 依赖xclip，同时截图工具如(spectacle)必须设置为复制截图到剪贴板或按下C-c来复制
-(use-package org-download
-  :ensure t
-  :defer nil
-  :after org)
-
-
-;; ========================================
 ;; 设置org-preview-html
 ;; ========================================
 (use-package org-preview-html
