a568cd076e82f8093ebce19b00a9eb0207d7894f
kle <thenum6er078@gmail.com>
2025-08-27 01:19:32 +0800

    [WSL GNU Emacs环境]: 设置了华氏温度F与摄氏温度C的转换设计

--- elisp/site-lisp/sh/weather-situation.sh
+++ elisp/site-lisp/sh/weather-situation.sh
@@ -3,7 +3,13 @@
 # Please save file called "weather" into ~/.emacs.d/archive.
 # And pull the latest data and save it if not updating. (or with use api payfully 󰫕)
 
-weather_temperature=$(cat ~/.emacs.d/archive/weather | awk -F'[()]' '{print $2}' | grep -oP '[0-9]+' | sed -n '4p')
+weather_temperature=0
+curl_weather_temperature=$(cat ~/.emacs.d/archive/weather | awk -F'[()]' '{print $2}' | grep -oP '[0-9]+' | sed -n '4p')
 weather_location1=$(cat ~/.emacs.d/archive/weather | sed -n '38p' | awk -F ', ' '{print $2}' | cut -c '1-9')
 weather_location2=$(cat ~/.emacs.d/archive/weather | sed -n '38p' | awk -F ', ' '{print $3}')
+
+if [ "$curl_weather_temperature" -gt 45 ]; then
+        weather_temperature=$(((curl_weather_temperature - 32) * 5 / 9))
+fi
+
 echo "$weather_temperature°C ($weather_location1, $weather_location2)"
