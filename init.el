628a50c61972de7d4d92458913788cf6e4256bd3
kle <thenum6er078@gmail.com>
2025-09-09 23:35:24 +0800

    [WSL GNU Emacs环境]: 更新了使用时长

--- init.el
+++ init.el
@@ -67,14 +67,7 @@
      "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
      "3001510be1be4a38508ff07633e97b810c0a52c6fa2f4c7153d22cc7596d9172"
      "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" default))
- '(package-selected-packages
-   '(auctex bliss-theme cdlatex centaur-tabs corfu dashboard deadgrep denote
-            dirvish eat eglot embark-consult emms evil highlight-indent-guides
-            magit marginalia nano-theme nerd-icons-completion nerd-icons-corfu
-            nerd-icons-dired nerd-icons-ibuffer orderless org-download
-            org-preview-html pdf-tools rainbow-identifiers restart-emacs rime
-            smartparens telega tree-sitter-langs valign vertico-posframe
-            yasnippet-snippets)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
