9318b8f9010720af37d1eec70e8e471b35c6281c
kle <thenum6er078@gmail.com>
2025-09-15 17:38:45 +0800

    [WSL GNU Emacs环境]: 优化了load-path加载目录的配置

--- elisp/basic/basic-path.el
+++ elisp/basic/basic-path.el
@@ -1,100 +1,30 @@
 ;; =======================================
 ;; 关于路径加载 basic-path.el
 ;; =======================================
-
-;; =======================================
-;; 关于基本设置路径
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/basic")))
-;; =======================================
-;; 关于自定义函数路径
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/custom")))
-;; =======================================
-;; 关于体验增强路径
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/enhance")))
-;; =======================================
-;; 关于ui路径
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/ui/font")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/ui/icon")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/ui/theme")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/ui/decorate")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/ui/modeline")))
-;; =======================================
-;; 关于笔记路径
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/note/orgmode")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/note/auctex")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/note/denote")))
-;; =======================================
-;; 关于编程路径
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/zprogramming/cmp")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/zprogramming/lsp")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/zprogramming/snips")))
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/zprogramming/ts")))
-;; =======================================
-;; 关于快捷键设置
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/key")))
-;; =======================================
-;; 关于快捷键设置
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/hooks")))
-;; =======================================
-;; 关于instant-messaging(IM)设置
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/im")))
-;; =======================================
-;; 关于site-lisp
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "site-lisp/sh")))
-;; =======================================
-;; 关于音乐播放器
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name
-              (concat user-emacs-directory "elisp/music")))
+(dolist (dir '(
+               "elisp/basic"               ; 关于基本设置路径
+               "elisp/custom"              ; 关于自定义函数路径
+               "elisp/enhance"             ; 关于体验增强路径
+               "elisp/ui/font"             ; 关于ui路径
+               "elisp/ui/icon"
+               "elisp/ui/theme"
+               "elisp/ui/decorate"
+               "elisp/ui/modeline"
+               "elisp/note/orgmode"        ; 关于笔记路径
+               "elisp/note/auctex"
+               "elisp/note/denote"
+               "elisp/zprogramming/cmp"    ; 关于编程路径
+               "elisp/zprogramming/lsp"
+               "elisp/zprogramming/snips"
+               "elisp/zprogramming/ts"
+               "elisp/key"                 ; 关于快捷键设置
+               "elisp/hooks"               ; 关于钩子设置
+               "elisp/im"                  ; 关于instant-messaging(IM)设置
+               "site-lisp/sh"              ; 关于site-lisp
+               "elisp/music"               ; 关于音乐播放器
+               ))
+  (add-to-list 'load-path
+               (expand-file-name dir user-emacs-directory)))
 
 
 (provide 'basic-path)
